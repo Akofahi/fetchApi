173f4d5ab535da33664863d6c0f0a6194ce57727
Ahmad AlKofahi <AKofahi@outlook.com>
2022-07-05 18:00:11 +0300

    Code refactoring

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ fetch('https://restcountries.com/v2/all')
         
         for (const key in data) {
 
+            //Country Name Fetcher
             const countryName = document.createElement('h2')
             countryName.classList.add("countryName");
             countryName.textContent = data[key].name;
 
+            //Country Flag Fetcher
             const countryFlag = document.createElement('img');
             countryFlag.classList.add("countryFlag");
             countryFlag.src = data[key].flags.png;
 
+            //Country Languages Fetcher
             const countryLanguage = document.createElement('h2');
             countryLanguage.classList.add("countryLanguage");
             countryLanguage.innerHTML = "Languages <br>"
@@ -19,14 +22,16 @@ fetch('https://restcountries.com/v2/all')
                 countryLanguage.innerHTML = countryLanguage.innerHTML + "  " + data[key].languages[lkey].name + "  ";
             }
 
-            const  gridElement = document.createElement('div');
-            gridElement.classList.add("grid-item");
-            gridElement.appendChild(countryName);
-            gridElement.appendChild(countryFlag);
-            gridElement.appendChild(countryLanguage);
+            //Constructing the Grid Item
+            const  gridItem = document.createElement('div');
+            gridItem.classList.add("grid-item");
+            gridItem.appendChild(countryName);
+            gridItem.appendChild(countryFlag);
+            gridItem.appendChild(countryLanguage);
 
+            //Adding the Grid Item to the Grid
             const grid = document.getElementsByClassName("grid-container");
-            grid[0].appendChild(gridElement);
+            grid[0].appendChild(gridItem);
 
 
 
